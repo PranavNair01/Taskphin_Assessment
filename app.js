6c64450efeac6e181e64cd54e1c37a815b66223e
PranavNair01 <nairpranav73@gmail.com>
2024-01-08 20:13:22 +0530

    Refactor code. Add Login, Register, Home views.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const { sq, testDbConnection } = require('./config/db');
 const { User } = require('./models/user');
+const authController = require('./controllers/authController');
 
+// Server App Configuration
 const app = express();
 const port = process.env.PORT || 3000;
 
+// View Engine
+app.set('view engine', 'ejs');
+
+// Server and DB Initialization
 testDbConnection().then(() => {
     app.listen(port, () => {
         console.log('Listening on port ' + port.toString());
     });
 });
+
+// Authentication Routes
+app.get('/register', authController.register_get);
+app.post('/register', authController.register_post);
+app.get('/login', authController.login_get);
+app.post('/login', authController.login_post);
+
+// Homepage Route
+app.get('/', (req, res) => {
+    res.render('home');
+})
---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+module.exports.register_get = (req, res) => {
+    res.render('register');
+}
+
+module.exports.register_post = (req, res) => {
+
+}
+
+module.exports.login_get = (req, res) => {
+    res.render('login');
+}
+
+module.exports.login_post = (req, res) => {
+
+}
